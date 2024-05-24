c35c4cc5e032f80dfc87d7272bfef6cc8ac37edc
Tiago Possato <dev.tiago.possato@gmail.com>
2024-05-24 19:34:10 -0300

    merge dev

--- modbus_getter/modbus_getter.py
+++ modbus_getter/modbus_getter.py
@@ -26,35 +26,6 @@ def transform_float_PotAtivT(value):
     """
     return transform_float(value) * 3
 
-<<<<<<< HEAD
-UrmsA = Point('UrmsA', base_address=68, count=2)
-UrmsA.set_transformer(transform_float)
-
-IrmsA = Point('IrmsA', base_address=74, count=2)
-IrmsA.set_transformer(transform_float)
-
-PotAtivA = Point('PotAtivA', base_address=80, count=2)
-PotAtivA.set_transformer(transform_float)
-
-PotAtivT = Point('PotAtivT', base_address=86, count=2)
-
-def transform_float_PotAtivT(value):
-    """
-    Estiva a potencia ativa total multiplicando a potencia da Fase Ativa atual por 3.
-    Atualmente a potencia ativa total é a mesma da Fase A, por que está sendo medido somente uma fase
-    """
-    return transform_float(value) * 3
-PotAtivT.set_transformer(transform_float_PotAtivT)
-
-PotReatA = Point('PotReatA', base_address=88, count=2)
-PotReatA.set_transformer(transform_float)
-
-PotAparA = Point('PotAparA', base_address=96, count=2)
-PotAparA.set_transformer(transform_float)
-
-FatPotA = Point('FatPotA', base_address=104, count=2)
-FatPotA.set_transformer(transform_float)
-=======
 FreqA = Point('FreqA', base_address=66, count=2, update_interval=5, transformer=transform_float)
 UrmsA = Point('UrmsA', base_address=68, count=2, update_interval=5, transformer=transform_float)
 UrmsB = Point('UrmsB', base_address=70, count=2, update_interval=5, transformer=transform_float)
@@ -65,7 +36,6 @@ PotReatA = Point('PotReatA', base_address=88, count=2, update_interval=5, transf
 PotAparA = Point('PotAparA', base_address=96, count=2, update_interval=5, transformer=transform_float)
 FatPotA = Point('FatPotA', base_address=104, count=2, update_interval=5, transformer=transform_float)
 PotAtivT = Point('PotAtivT', base_address=86, count=2, update_interval=5, transformer=transform_float_PotAtivT)
->>>>>>> dev
 
 
 MMDemP = Point('MMDemP', base_address=602, count=2)
